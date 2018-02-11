6e6029a595c4ba759e05c10cd413db957baf838f
bspns073 <bspns073@gmail.com>
2018-02-11 15:43:14 -0800

    Update ec2.py

--- project/ec2.py
+++ project/ec2.py
@@ -3,35 +3,24 @@
 '''
 EC2 external inventory script
 =================================
-
 Generates inventory that Ansible can understand by making API request to
 AWS EC2 using the Boto library.
-
 NOTE: This script assumes Ansible is being executed where the environment
 variables needed for Boto have already been set:
     export AWS_ACCESS_KEY_ID='AK123'
     export AWS_SECRET_ACCESS_KEY='abc123'
-
 optional region environment variable if region is 'auto'
-
 This script also assumes there is an ec2.ini file alongside it.  To specify a
 different path to ec2.ini, define the EC2_INI_PATH environment variable:
-
     export EC2_INI_PATH=/path/to/my_ec2.ini
-
 If you're using eucalyptus you need to set the above variables and
 you need to define:
-
     export EC2_URL=http://hostname_of_your_cc:port/services/Eucalyptus
-
 If you're using boto profiles (requires boto>=2.24.0) you can choose a profile
 using the --boto-profile command line argument (e.g. ec2.py --boto-profile prod) or using
 the AWS_PROFILE variable:
-
     AWS_PROFILE=prod ansible-playbook -i ec2.py myplaybook.yml
-
 For more details, see: http://docs.pythonboto.org/en/latest/boto_config_tut.html
-
 When run against a specific host, this script returns the following variables:
  - ec2_ami_launch_index
  - ec2_architecture
@@ -89,16 +78,13 @@ When run against a specific host, this script returns the following variables:
  - ec2_tenancy
  - ec2_virtualization_type
  - ec2_vpc_id
-
 These variables are pulled out of a boto.ec2.instance object. There is a lack of
 consistency with variable spellings (camelCase and underscores) since this
 just loops through all variables the object exposes. It is preferred to use the
 ones with underscores when multiple exist.
-
 In addition, if an instance has AWS Tags associated with it, each tag is a new
 variable named:
  - ec2_tag_[Key] = [Value]
-
 Security groups are comma-separated in 'ec2_security_group_ids' and
 'ec2_security_group_names'.
 '''
